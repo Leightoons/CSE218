0a813762471d4420035ae361e1dab014e1e25aae
Leightoons <65992785+Leightoons@users.noreply.github.com>
2024-10-08 14:33:55 -0400

    added a whole bunch of majors

--- Course Project/src/model/Major.java
+++ Course Project/src/model/Major.java
@@ -1,11 +1,41 @@
 package model;
 
 public enum Major {
-	CSE("Computer Science"),
+	// Business
+	ACC("Accounting"),
+	BUS("Business Administration"),
+	// Humanities
 	ENG("English"),
-	MAT("Math"),
-	SCI("Science"),
+	MUS("Music"),
+	PHL("Philosophy"),
+	ART("Visual Arts"),
+	// Physical Education
+	EXS("Exercise Science"),
+	NUR("Nursing"),
+	PAR("Paramedic"),
+	PED("Physical Education"),
+	// Science & Math
+	AST("Astronomy"),
+	BIO("Biology"),
+	CHE("Chemistry"),
+	MAT("Mathematics"),
+	PHY("Physics"),
+	// Engineering Science/Tech
+	CSE("Computer Science"),
+	CYB("Cybersecurity"),
+	ELT("Electrical Engineering"),
+	ENS("Engineering Science"),
+	// Social Sciences
+	ECO("Economics"),
+	GEO("Geography"),
 	HIS("History"),
+	POL("Political Science"),
+	PSY("Psychology"),
+	SOC("Sociology"),
+	// Special Areas
+	CRJ("Criminal Justice"),
+	EDU("Education"),
+	VST("Veterinary Science")
 	;
 	
 	public final String label;
